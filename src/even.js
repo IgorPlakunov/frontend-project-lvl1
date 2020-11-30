b1a63ac846cc7631bc519cf4d80c60d06d0de3ad
Plakunov <igorplakunov@gmail.com>
2020-11-30 19:58:18 +0300

    brain-even with recursion

--- src/even.js
+++ src/even.js
@@ -4,28 +4,34 @@ import promptly from 'promptly';
 
 const numberRange = 100;
 const rounds = 3;
-const numbers = [];
 
 const getRandomNumber = () => Math.floor(Math.random() * numberRange);
 
-for (let i = 0; i < rounds; i++) {
-  const randomNumber = getRandomNumber();
-  numbers.push(randomNumber);
-}
-
-const isEven = (randomNumber) => (randomNumber % 2 === 0 ? 'yes' : 'no');
+const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
 
 export default async () => {
   console.log('Welcome to the Brain Games!');
   const name = await promptly.prompt('May I have your name?');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no"');
-  numbers.forEach(async (item) => {
-    console.log(`Question: ${item}`);
-    await promptly.prompt('Your answer:');
-    if (isEven(item) === userAnswer) {
-      console.log('Correct!');
-    } return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven(item)}'. Let's try again, ${name}!`);
-  });
-  return console.log(`Congratulations, ${name}!`);
+
+  function startGame() {
+    async function startRounds(currentRounds) {
+      if (!currentRounds) {
+        return console.log(`Congratulations, ${name}!`);
+      }
+      const randomNumber = getRandomNumber();
+      console.log(`Question: ${randomNumber}`);
+      const userAnswer = await promptly.prompt('Your answer:');
+      if (isEven(randomNumber) === userAnswer) {
+        console.log('Correct!');
+      } else {
+        return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven(randomNumber)}'. Let's try again, ${name}!`);
+      }
+      startRounds(currentRounds - 1);
+    }
+    startRounds(rounds);
+  }
+
+  startGame();
 };
